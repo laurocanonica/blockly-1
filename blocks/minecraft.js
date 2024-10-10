4ccf6a9eff0c14b2e68a5596b72ae1e8b9151955
laurocanonica <lauro.canonica@gmail.com>
2024-10-10 19:38:03 +0200

    fixed the "multiplicity" labeling

--- blocks/minecraft.js
+++ blocks/minecraft.js
@@ -1767,7 +1767,7 @@ Blockly.Blocks['minecraft_multiciplity_ver2'] = {
 		init : function() {
 			this.jsonInit({
 			  "type": "minecraft_multiciplity_ver2",
-			  "message0": "%1 of %2 %3",
+			  "message0": eval('Blockly.Msg.MC_cmd_minecraft_multiciplity'+'_'+GLOBAL_MESSAGE_VERSION),
 			  "args0": [
 			    {
 			      "type": "input_value",
