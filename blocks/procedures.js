27abfd994791b450d965f21e26d0e0c3247bb274
laurocanonica <lauro.canonica@gmail.com>
2024-12-01 14:41:23 +0100

    moved parameters

--- blocks/procedures.js
+++ blocks/procedures.js
@@ -426,7 +426,7 @@ var saveOption = {
         console.log('Current Blockly language:', language);
 
         // Generate the viewer URL
-		var viewerUrl = 'blockly_viewer.html?xml=' + encodedXml + '&filename=function_screenshot&language=' + language;
+		var viewerUrl = 'blockly_viewer.html?filename=function_screenshot&language=' + language+'&xml=' + encodedXml;;
         // Use Clipboard API to copy the XML text
         if (navigator.clipboard && navigator.clipboard.writeText) {
             navigator.clipboard.writeText(encodedXml)
