028541b9c78d1b7df67e8d24dc39811d39a4d81f
laurocanonica <lauro.canonica@gmail.com>
2025-04-08 17:48:12 +0200

    file tha handle symbol lists

---
+++ blocks/FieldUnicodeGrid.js
@@ -0,0 +1,50 @@
+function FieldUnicodeGrid(symbolGrid) {
+  // Flatten grid into dropdown-compatible options
+  var flatOptions = [];
+  for (var i = 0; i < symbolGrid.length; i++) {
+    for (var j = 0; j < symbolGrid[i].length; j++) {
+      flatOptions.push([symbolGrid[i][j], symbolGrid[i][j]]);
+    }
+  }
+  Blockly.FieldDropdown.call(this, flatOptions);
+  this.symbolGrid_ = symbolGrid;
+}
+FieldUnicodeGrid.prototype = Object.create(Blockly.FieldDropdown.prototype);
+FieldUnicodeGrid.prototype.constructor = FieldUnicodeGrid;
+
+FieldUnicodeGrid.prototype.showEditor_ = function() {
+  Blockly.FieldDropdown.prototype.showEditor_.call(this);
+  var self = this;
+  setTimeout(function() {
+    var menu = document.querySelector('.blocklyMenu');
+    if (!menu) return;
+    menu.innerHTML = ''; // Clear default list
+
+    for (var i = 0; i < self.symbolGrid_.length; i++) {
+      var rowDiv = document.createElement('div');
+      rowDiv.style.display = 'flex';
+      rowDiv.style.margin = '2px 0';
+
+      for (var j = 0; j < self.symbolGrid_[i].length; j++) {
+        (function(symbol) {
+          var item = document.createElement('div');
+          item.textContent = symbol;
+          item.style.padding = '6px 10px';
+          item.style.fontSize = '20px';
+          item.style.cursor = 'pointer';
+          item.style.border = '1px solid #ccc';
+          item.style.marginRight = '4px';
+          item.style.borderRadius = '4px';
+
+          item.onclick = function() {
+            self.setValue(symbol);
+            Blockly.DropDownDiv.hideIfOwner(self);
+          };
+
+          rowDiv.appendChild(item);
+        })(self.symbolGrid_[i][j]);
+      }
+      menu.appendChild(rowDiv);
+    }
+  }, 1);
+};
